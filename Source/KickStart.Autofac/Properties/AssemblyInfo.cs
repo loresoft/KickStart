44f4be89266ee73b35e81ea05f10df98999e85c0
paul.welter <paul.welter@agentimediaservices.com>
2014-06-10 14:42:46 -0500

    update build

--- Source/KickStart.Autofac/Properties/AssemblyInfo.cs
+++ Source/KickStart.Autofac/Properties/AssemblyInfo.cs
@@ -2,35 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("KickStart.Autofac")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Novus Media Inc")]
-[assembly: AssemblyProduct("KickStart.Autofac")]
-[assembly: AssemblyCopyright("Copyright © Novus Media Inc 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyDescription("KickStart Extension for Autofac")]
 [assembly: Guid("bc7d4597-65a4-4daf-a733-cfa97049bdb7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
