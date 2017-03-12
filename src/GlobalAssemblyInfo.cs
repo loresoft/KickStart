6b3c860a6c8e409dc108f93389f345acf03ad2e3
Paul Welter <pwelter@loresoft.com>
2017-03-11 18:58:24 -0600

    update packages

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -12,9 +12,9 @@
 [assembly: System.Reflection.AssemblyCompany("LoreSoft")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright © 2017 LoreSoft")]
 [assembly: System.Reflection.AssemblyConfiguration("Release")]
-[assembly: System.Reflection.AssemblyVersion("4.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("4.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("4.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("4.1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("4.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("4.1.0.0")]
 
 
 
@@ -28,11 +28,11 @@ internal sealed partial class ThisAssembly {
     
     internal const string AssemblyConfiguration = "Release";
     
-    internal const string AssemblyVersion = "4.0.0.0";
+    internal const string AssemblyVersion = "4.1.0.0";
     
-    internal const string AssemblyFileVersion = "4.0.0.0";
+    internal const string AssemblyFileVersion = "4.1.0.0";
     
-    internal const string AssemblyInformationalVersion = "4.0.0.0";
+    internal const string AssemblyInformationalVersion = "4.1.0.0";
     
     private ThisAssembly() {
     }
